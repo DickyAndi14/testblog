32e8b6d0aeabaa636c466bb880cfe9f1aa06a002
DICKY ANDI S <71749211+DickyAndi14@users.noreply.github.com>
2020-11-20 17:08:18 +0700

    Update models.py

--- myaccount/models.py
+++ myaccount/models.py
@@ -15,7 +15,7 @@ class Image(models.Model):
         ('Gitar Classic', 'Gitar Classic'),
         )
     nama = models.CharField(max_length=1000) 
-    deskripsi = models.CharField(max_length=10000000000000)
+    deskripsi = models.CharField(max_length=4000)
     Tags= models.ManyToManyField(Tag)
     Category = models.CharField(max_length= 1000, null=True, choices=CATEGORY)
     image = models.ImageField(upload_to='static/%y')
